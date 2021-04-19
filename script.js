ca5607399f22b8ad56944b5a6e29887476970d4e
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-19 17:13:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const age = 17;
+
+if (age > 17) {
+ console.log("You may enter")
+} else {
+ console.log("Please stay outside")
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Buy here your Ladiesnight tickets")
+} else {
+    console.log("Sorry, this event is not available for you")
+}
+
+const driverStatus = "Bob";
+
+if (driverStatus == "Bob") {
+    console.log("You are allowd to drive")
+} else {
+    console.log("You are not allowed to drive")
+}
